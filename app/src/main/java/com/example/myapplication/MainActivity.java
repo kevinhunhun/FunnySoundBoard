7503e063dbbef45bfd290e9603992f3b70e64c43
201001024 <hun2fun@gmail.com>
2023-05-31 13:55:02 -0700

    choirchoir

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -41,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mpBackTrack;
 
+    private MediaPlayer mpChoir;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
